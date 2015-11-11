6b779cbf7143934f8e93bf6199f3353d4a5b454e
A5-Renegado <iangeasterbrook@gmail.com>
2015-11-11 06:26:34 -0500

    Update main.cpp
    Software prototype 2, for Panel exam.
    Removed code for streaming video to screen, added RGB code processing to determine what pixels contain parts of peaches.

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@ extern "C" {
 }
 #endif
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #define width 640
 #define height 480
 #define GL_BGRA 0x80E1
@@ -25,7 +29,54 @@ GLubyte data[width*height * 4];
 
 //Kinect variables
 HANDLE rgbStream;
+HANDLE depthStream;
 INuiSensor* sensor;
+
+//Global Variables
+long depthToRgbMap[width*height * 2];
+float colorarray[width*height * 3];
+float vertexarray[width*height * 3];
+int locationpeaches[width*height];
+int counter;
+float redmin = 0.5568627; // 0.5568627
+float redmax = 0.9568627;
+float greenmin = 0.3490196; // 0.3490196
+float greenmax = 0.7647059;
+float bluemin = 0.4352941; // 0.4352941
+float bluemax = 0.7882353;
+
+//Testing variables
+bool end;
+bool print;
+std::ofstream myfile;
+std::ofstream depthfile;
+std::ofstream peachfile;
+
+//Function to determine if a set of RGB codes might define a pixel
+int checkPixel(float red, float green, float blue)
+{
+	if (red <= redmax && red >= redmin && green <= greenmax && green >= greenmin && blue <= bluemax && blue >= bluemin)
+	{
+		return 1;
+	}
+	return 0;
+}
+
+// Function to determine what pixels contain part of a peach
+bool checkImage()
+{
+	bool peaches = false;
+	for (int i = 0; i < width*height; i++)
+	{
+		int isPixel = checkPixel(colorarray[i * 3], colorarray[i * 3 + 1], colorarray[i * 3 + 2]);
+		if (isPixel == 1)
+		{
+			peaches = true;
+		}
+		locationpeaches[i] = isPixel;
+	}
+	return peaches;
+}
 
 // Function to connect to a Kinect sensor
 bool initKinect()
@@ -36,66 +87,195 @@ bool initKinect()
 	if (NuiCreateSensorByIndex(0, &sensor) < 0) return false;
 
 	// Initialize sensor
-	sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH | NUI_INITIALIZE_FLAG_USES_COLOR);
-	sensor->NuiImageStreamOpen(
+	if (sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH | NUI_INITIALIZE_FLAG_USES_COLOR) < 0)
+	{
+		std::string fail;
+		std::cout << "Failed to initialize" << std::endl;
+		std::cin >> fail;
+		return false;
+	}
+	long rgbSuccess = sensor->NuiImageStreamOpen(
 		NUI_IMAGE_TYPE_COLOR,			// Depth camera or rgb camera?
 		NUI_IMAGE_RESOLUTION_640x480,	// Image resolution
-		0,		// Image stream fialgs
-		2,		// Number of frames to buffer
+		0,		// Image stream flags
+		1,		// Number of frames to buffer
 		NULL,	//Event handle
 		&rgbStream);
+	long depthSuccess = sensor->NuiImageStreamOpen(
+		NUI_IMAGE_TYPE_DEPTH,			// Depth camera or rgb camera?
+		NUI_IMAGE_RESOLUTION_640x480,	// Image resolution
+		0,		// Image stream flags
+		1,		// Number of frames to buffer
+		NULL,	//Event handle
+		&depthStream);
+	std::cout << sensor << std::endl << rgbSuccess << std::endl << depthSuccess << std::endl;
+	std::cout << E_FAIL << " " << E_INVALIDARG << " " << E_NUI_DEVICE_NOT_READY << " " << E_OUTOFMEMORY << " " << E_POINTER << std::endl;
 	return sensor;
 }
 
-// Function to get frame from Kinect
-void getKinectData(GLubyte* dest)
+void getRgbData(GLubyte* dest)
 {
 	NUI_IMAGE_FRAME imageFrame;
 	NUI_LOCKED_RECT LockedRect;
-	if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0) return;
+	if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0)
+	{
+		std::cout << "Could not get next frame (RGB)" << std::endl;
+		print = false;
+		return;
+	}
 	INuiFrameTexture* texture = imageFrame.pFrameTexture;
 	texture->LockRect(0, &LockedRect, NULL, 0);
 	if (LockedRect.Pitch != 0)
 	{
-		const BYTE* curr = (const BYTE*)LockedRect.pBits;
-		const BYTE* dataEnd = curr + (width*height) * 4;
-
-		while (curr < dataEnd)
+		std::cout << "Doing stuff" << std::endl;
+		const BYTE* start = (const BYTE*)LockedRect.pBits;
+		float* fdest = (float*)dest;
+		long* depth2rgb = (long*)depthToRgbMap;
+		for (int j = 0; j < height; ++j) 
 		{
-			*dest++ = *curr++;
+			for (int i = 0; i < width; ++i) 
+			{
+				// Determine color pixel for depth pixel i,j
+				long x = *depth2rgb++;
+				long y = *depth2rgb++;
+				// If out of bounds, then do not color this pixel
+				if (x < 0 || y < 0 || x > width || y > height) {
+					for (int n = 0; n < 3; ++n) *fdest++ = 0.f;
+				}
+				else {
+					// Determine rgb color for depth pixel (i,j) from color pixel (x,y)
+					const BYTE* color = start + (x + width*y) * 4;
+					if (color) print = true;
+					for (int n = 0; n < 3; ++n)
+					{
+						*fdest++ = color[2 - n] / 255.f;
+					}
+				}
+			}
 		}
 	}
 	texture->UnlockRect(0);
 	sensor->NuiImageStreamReleaseFrame(rgbStream, &imageFrame);
 }
 
+void getDepthData(GLubyte* dest)
+{
+	NUI_IMAGE_FRAME imageFrame;
+	NUI_LOCKED_RECT LockedRect;
+	if (sensor->NuiImageStreamGetNextFrame(depthStream, 0, &imageFrame) < 0)
+	{
+		std::cout << "Could not get next frame (Depth)" << std::endl;
+		return;
+	}
+	INuiFrameTexture* texture = imageFrame.pFrameTexture;
+	texture->LockRect(0, &LockedRect, NULL, 0);
+	if (LockedRect.Pitch != 0)
+	{
+		const USHORT* curr = (const USHORT*)LockedRect.pBits;
+		float* fdest = (float*)dest;
+		long* depth2rgb = (long*) depthToRgbMap;
+		for (int j = 0; j < height; ++j)
+		{
+			for (int i = 0; i < width; ++i)
+			{
+				USHORT depth = NuiDepthPixelToDepth(*curr);
+				Vector4 pos = NuiTransformDepthImageToSkeleton(i, j, *curr);
+				*fdest++ = pos.x / pos.w;
+				*fdest++ = pos.y / pos.w;
+				*fdest++ = pos.z / pos.w;
+				if ((pos.x / pos.w) != 0 || (pos.y / pos.w) != 0 || (pos.z / pos.w) != 0)
+				{
+					//end = true;
+				}
+				NuiImageGetColorPixelCoordinatesFromDepthPixelAtResolution(
+					NUI_IMAGE_RESOLUTION_640x480,
+					NUI_IMAGE_RESOLUTION_640x480,
+					NULL,
+					i, j, *curr,
+					depth2rgb, depth2rgb + 1
+				);
+				depth2rgb += 2;
+				*curr++;
+			}
+		}
+	}
+	texture->UnlockRect(0);
+	sensor->NuiImageStreamReleaseFrame(depthStream, &imageFrame);
+}
+
+// Function to get frame from Kinect
+void getKinectData()
+{
+	getDepthData((GLubyte*)vertexarray);
+	getRgbData((GLubyte*)colorarray);
+}
+
 void drawKinectData()
 {
-	glBindTexture(GL_TEXTURE_2D, textureId);
-	getKinectData(data);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)data);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_QUADS);
-		glTexCoord2f(0.0f, 0.0f);
-		glVertex3f(0, 0, 0);
-		glTexCoord2f(1.0f, 0.0f);
-		glVertex3f(width, 0, 0);
-		glTexCoord2f(1.0f, 1.0f);
-		glVertex3f(width, height, 0.0f);
-		glTexCoord2f(0.0f, 1.0f);
-		glVertex3f(0, height, 0.0f);
-	glEnd();
+	bool peaches = false;
+	getKinectData();
+	if (print)
+	{
+		peaches = checkImage();
+		std::string colorfilename = "SampleColorData";
+		std::string depthfilename = "SampleDepthData";
+		std::string peachfilename = "SamplePeachData";
+		//char buffer[33];
+		//char *num = _itoa(j, buffer, 10);
+		colorfilename.append(std::to_string(counter));
+		depthfilename.append(std::to_string(counter));
+		peachfilename.append(std::to_string(counter));
+		depthfilename.append(".txt");
+		colorfilename.append(".txt");
+		peachfilename.append(".ppm");
+		myfile.open(colorfilename);
+		depthfile.open(depthfilename);
+		//peachfile.open(peachfilename);
+		FILE *f = fopen("SamplePeachData.ppm", "wb");
+		fprintf(f, "P6\n%i %i 255\n", width, height);
+		for (int i = 0; i < width*height; i++)
+		{
+			myfile << colorarray[i * 3] << " " << colorarray[i * 3 + 1] << " " << colorarray[i * 3 + 2] << " ";
+			depthfile << vertexarray[i*3] << " " << vertexarray[i * 3 + 1] << " " << vertexarray[i * 3 + 2] << " ";
+			fputc(locationpeaches[i] * 255, f);
+			fputc(locationpeaches[i] * 255, f);
+			fputc(locationpeaches[i] * 255, f);
+			//peachfile << locationpeaches[i];
+			//if ((i + 1) % width == 0)
+			//{
+			//	peachfile << std::endl;
+			//}
+		}
+		myfile.close();
+		depthfile.close();
+		fclose(f);
+		//peachfile.close();
+		//j++;
+	}
 }
 
 void draw()
 {
 	drawKinectData();
-	glutSwapBuffers();
 }
 
 void execute()
 {
-	glutMainLoop();
+	bool execution = true;
+	std::string test;
+	counter = 0;
+	while (execution)
+	{
+		//std::cout << "In Execution Loop" << std::endl;
+		draw();
+		if (end)
+		{
+			execution = false;
+		}
+		std::cout << "Testing pacer, press enter to continue: ";
+		std::cin >> test;
+		counter++;
+	}
 }
 
 bool init(int argc, char* argv[])
@@ -111,32 +291,10 @@ bool init(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
-	if (!init(argc, argv)) return 1;
 	if (!initKinect()) return 1;
 
-	// Initialize textures
-	glGenTextures(1, &textureId);
-	glBindTexture(GL_TEXTURE_2D, textureId);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height,
-		0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)data);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	// OpenGL setup
-	glClearColor(0, 0, 0, 0);
-	glClearDepth(1.0f);
-	glEnable(GL_TEXTURE_2D);
-
-	// Camera setup
-	glViewport(0, 0, width, height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, width, height, 0, 1, -1);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
 	// Main Loop
+	print = false;
 	execute();
 	return 0;
 }
