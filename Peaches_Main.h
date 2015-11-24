ecf4217d4e66a25d50c10d4533a63044e9f6b2f3
A5-Renegado <iangeasterbrook@gmail.com>
2015-11-23 21:35:22 -0500

    Create Peaches_Main.h as part of refactoring

---
+++ Peaches_Main.h
@@ -0,0 +1,17 @@
+#include "Kinect_ImProc.h"
+
+
+class Peaches_Main
+{
+public:
+	int execute();
+	void set_debug_mode(bool debug);
+	bool get_debug_mode();
+	Peaches_Main(bool debug);
+private:
+	bool debug_mode;
+	int main_loop();
+	int counter;
+	bool execution;
+	Kinect_ImProc image_processing;
+};
