e8a4e737101ee041ac12a3138cebf63c1f71b780
A5-Renegado <iangeasterbrook@gmail.com>
2015-12-04 13:23:14 -0500

    Create ThreeDPos.h

---
+++ ThreeDPos.h
@@ -0,0 +1,16 @@
+class ThreeDPos
+{
+public:
+	ThreeDPos();
+	double getX();
+	double getY();
+	double getZ();
+
+	void setX(double in);
+	void setY(double in);
+	void setZ(double in);
+private:
+	double x;
+	double y;
+	double z;
+};
