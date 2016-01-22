9fc42883e732e9337067919d9b2068804405ebb5
A5-Renegado <iangeasterbrook@gmail.com>
2016-01-22 13:26:26 -0500

    CLR Commit

---
+++ Peaches__CLR2/ThreeDPos.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "ThreeDPos.h"
+
+ThreeDPos::ThreeDPos()
+{
+	x = 0;
+	y = 0;
+	z = 0;
+}
+
+double ThreeDPos::getX()
+{
+	return x;
+}
+
+double ThreeDPos::getY()
+{
+	return y;
+}
+
+double ThreeDPos::getZ()
+{
+	return z;
+}
+
+void ThreeDPos::setX(double in)
+{
+	x = in;
+}
+
+void ThreeDPos::setY(double in)
+{
+	y = in;
+}
+
+void ThreeDPos::setZ(double in)
+{
+	z = in;
+}
+
